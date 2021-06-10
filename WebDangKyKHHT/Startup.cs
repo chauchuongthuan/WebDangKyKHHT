b1e65fe70b6c61ea8e35b8cceda30da1195661ec
187PM06720 - CHAU CHUONG THUAN - T5 <thuan.187pm06720@vanlanguni.vn>
2021-06-10 15:07:11 +0700

    Empty Project

---
+++ WebDangKyKHHT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebDangKyKHHT.Startup))]
+namespace WebDangKyKHHT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
