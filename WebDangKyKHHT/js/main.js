42565bd573e9b89f66a6ce2900e7f84987d046a4
phuong187pm20547 <phuong.187pm20547@vanlanguni.vn>
2021-06-27 21:01:27 +0700

    Edit Template

--- WebDangKyKHHT/js/main.js
+++ WebDangKyKHHT/js/main.js
@@ -367,3 +367,40 @@ jQuery(document).ready(function($) {
 });
 
 });
+
+$(function () {
+
+	'use strict';
+
+	$('.js-menu-toggle').click(function (e) {
+
+		var $this = $(this);
+
+
+
+		if ($('body').hasClass('show-sidebar')) {
+			$('body').removeClass('show-sidebar');
+			$this.removeClass('activenav');
+		} else {
+			$('body').addClass('show-sidebar');
+			$this.addClass('activenav');
+		}
+
+		e.preventDefault();
+
+	});
+
+	// click outisde offcanvas
+	$(document).mouseup(function (e) {
+		var container = $(".sidebar");
+		if (!container.is(e.target) && container.has(e.target).length === 0) {
+			if ($('body').hasClass('show-sidebar')) {
+				$('body').removeClass('show-sidebar');
+				$('body').find('.js-menu-toggle').removeClass('activenav');
+			}
+		}
+	});
+
+
+
+});
