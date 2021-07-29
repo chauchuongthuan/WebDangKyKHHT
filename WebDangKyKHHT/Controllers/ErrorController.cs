651b7348da30400d281cb8c19e7f6300a82bdec1
shibarus <longch.dev@gmail.com>
2021-07-29 15:24:28 +0700

    Error404

---
+++ WebDangKyKHHT/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDangKyKHHT.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Regular Error";
+            return View();
+        }
+        public ActionResult NotFound404()
+        {
+            ViewBag.Title = "Error 404 - File not Found";
+            return View("Index");
+        }
+    }
+}
